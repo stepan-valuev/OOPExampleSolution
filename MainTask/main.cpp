29f1a8b893627ca939befb917defb8f82edfe8a4
Stepan Valuev <litovets008@gmail.com>
2025-04-08 18:46:35 +0300

    Test operator = with object pointers протестировали

--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -3,28 +3,28 @@
 int main() {
 	int n = 0;
 
-	Student st1, st2;
+	Student* st1 = nullptr;
+	Student* st2 = nullptr;
 
-	st1.name = "Stepan";
-	st1.age = 15;
-	st1.mark = 8.1;
-	st1.alive = true;
+	st1 = new Student;
 
-	st2.name = "Danik";
-	st2.age = 15;
-	st2.mark = 9.99;
-	st2.alive = false;
+	st1->name = "Stepan";
+	st1->age = 15;
+	st1->mark = 8.1;
+	st1->alive = true;
+
+	st2 = st1;
 
 	cout << "Before: " << endl;
-	cout << st1.getString() << endl;
-	cout << st2.getString() << endl;
+	cout << st1->getString() << endl;
+	cout << st2->getString() << endl;
 
 	st1 = st2;
-	st1.name = "Bogdan";
+	st1->name = "Bogdan";
 
 	cout << "After: " << endl;
-	cout << st1.getString() << endl;
-	cout << st2.getString() << endl;
+	cout << st1->getString() << endl;
+	cout << st2->getString() << endl;
 
 	return 0;
 }
