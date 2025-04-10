074b40ccba9c5584f2e4f3b11397f1dc833ee4c4
Stepan Valuev <litovets008@gmail.com>
2025-04-10 17:29:47 +0300

    Add default-constructor in Student class

--- MainTask/Student.h
+++ MainTask/Student.h
@@ -10,8 +10,21 @@ public:
 	double mark;
 	bool alive;
 
+	//constructors
+
+	//deffault constructors or constructors without arguments
+	Student() {
+		cout << "deffault-constructor..." << endl;
+		name = "undefine";
+		age = 13;
+		mark = 4.0;
+		alive = false;
+	 
+	}
+
+
 	//methods
-	string getString() {
+	string toString() {
 		string s = "Name: " + name;
 		s += "; Age: " + to_string(age)
 		+ "; Average mark: " + to_string(mark)
--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -1,30 +1,16 @@
 #include "Student.h"
 
 int main() {
-	int n = 0;
+	Student student1{};
 
-	Student* st1 = nullptr;
-	Student* st2 = nullptr;
+	cout << "Before: " << student1.toString() << endl;
 
-	st1 = new Student;
+	student1.name = "Alex";
+	student1.age = 14;
+	student1.mark = 8.9;
+	student1.alive = true;
 
-	st1->name = "Stepan";
-	st1->age = 15;
-	st1->mark = 8.1;
-	st1->alive = true;
-
-	st2 = st1;
-
-	cout << "Before: " << endl;
-	cout << st1->getString() << endl;
-	cout << st2->getString() << endl;
-
-	st1 = st2;
-	st1->name = "Bogdan";
-
-	cout << "After: " << endl;
-	cout << st1->getString() << endl;
-	cout << st2->getString() << endl;
+	cout << "After: " << student1.toString() << endl;
 
 	return 0;
 }
